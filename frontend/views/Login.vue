9565c8b3f1ac64b2cb0982fe71affbc065352d68
Jolayemi Samuel <31437358+Jolayemisamuel@users.noreply.github.com>
2024-11-11 10:05:19 +0100

    Update Login.vue

--- frontend/views/Login.vue
+++ frontend/views/Login.vue
@@ -9,7 +9,7 @@
         <form @submit.prevent="login">
           <div class="box">
             <div class="has-text-centered">
-              <img src="/dist/img/logo.svg" class="logo">
+              <img :src="$store.state.config.logo" class="logo">
             </div>
             <br>
             <b-field :label="lang('Username')">
